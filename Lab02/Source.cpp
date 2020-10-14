495b5e71293696e64f9bddebf4ead45f782f5947
UniqueElect <randomnayapo4ta@gmail.com>
2020-10-14 22:11:56 +0300

    first branch_02 commit

--- Lab02/Source.cpp
+++ Lab02/Source.cpp
@@ -3,14 +3,14 @@ using namespace std;
 int main()
 {
 	double a; 
-  double z1; 
-	 //double z2; 
+  //double z1; 
+	 double z2; 
 	cout << "a = "; cin >> a;
-	 z1 = (sin(2 * a) + sin(5 * a) - sin(3 * a)) / (cos(a) + 1 - 2 * sin(2 * a) * sin(2 * a));
-	 //z2 = 2*sin(a);
+	//z1 = (sin(2 * a) + sin(5 * a) - sin(3 * a)) / (cos(a) + 1 - 2 * sin(2 * a) * sin(2 * a));
+	 z2 = 2*sin(a);
 	cout << endl;
-  cout << "z1 = " << z1 << endl;
-    //cout << "z2 = " << z2 << endl;
+  //cout << "z1 = " << z1 << endl;
+ cout << "z2 = " << z2 << endl;
 	cin.get();
 	return 0;
 }
